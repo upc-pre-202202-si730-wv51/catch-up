3e8abfd3dbbf44ee5534294eb80cb871f88bc84d
Angel Velasquez <pcisavel@upc.edu.pe>
2022-09-13 13:34:21 -0500

    feat: Added UI Components and Theme

--- src/main.js
+++ src/main.js
@@ -5,7 +5,33 @@ import './assets/main.css'
 import i18n from "@/i18n";
 import PrimeVue from "primevue/config";
 
+// App Theme
+import 'primevue/resources/themes/md-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+// Add PrimeFlex
+import 'primeflex/primeflex.css';
+
+// Add Components
+import Card from "primevue/card";
+import Button from "primevue/button";
+import SelectButton from "primevue/selectbutton";
+import Sidebar from "primevue/sidebar";
+import Avatar from "primevue/avatar";
+import Menu from "primevue/menu";
+import Menubar from "primevue/menubar";
+import Toolbar from "primevue/toolbar";
+
 createApp(App)
-    .use(PrimeVue)
+    .use(PrimeVue, { ripple: true })
     .use(i18n)
+    .component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-toolbar', Toolbar)
     .mount('#app');
